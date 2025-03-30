d89a80949498d75a7aeed9904ebf8be10480a339
Jing Li <abc1870819884@gmail.com>
2025-03-31 00:05:14 +0800

    update backend

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,15 @@
-def main():
-    print("Hello from backend!")
+from typing import Union
+
+from fastapi import FastAPI
+
+app = FastAPI()
 
 
-if __name__ == "__main__":
-    main()
+@app.get("/")
+async def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
