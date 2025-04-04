2160f253c3549d02a851f237536274be296982d2
Jing Li <abc1870819884@gmail.com>
2025-04-04 10:29:02 +0800

    add github repo fetcher

--- backend/rag.py
+++ backend/rag.py
@@ -1,26 +1,60 @@
 import dotenv
 
 dotenv.load_dotenv()
-from langchain_openai import ChatOpenAI
+import nest_asyncio
+from llama_index.llms.openai import OpenAI
+from llama_index.core import Settings
+nest_asyncio.apply()
+from llama_index.core import VectorStoreIndex
+from llama_index.readers.github import GithubRepositoryReader, GithubClient
+import os
+github_token = os.environ.get("GITHUB_TOKEN")
+openai_api_base = os.environ.get("OPENAI_API_BASE")
+openai_api_key = os.environ.get("OPENAI_API_KEY")
+owner = "stepbystepcode"
+repo = "CodeWay"
+branch = "main"
 
-chat = ChatOpenAI(model="gpt-4o-mini", temperature=0.2)
-from langchain_community.document_loaders import WebBaseLoader
+github_client = GithubClient(github_token=github_token, verbose=True)
+Settings.llm = OpenAI(model="gpt-4o", temperature=0.2, api_base=openai_api_base, api_key=openai_api_key)
+documents = GithubRepositoryReader(
+    github_client=github_client,
+    owner=owner,
+    repo=repo,
+    use_parser=True,  # Set to True to parse Markdown content into Document objects
+    verbose=False,
+    filter_directories=(
+        ["docs"],
+        GithubRepositoryReader.FilterType.INCLUDE,
+    ),
+    filter_file_extensions=(
+        [".md"],
+        GithubRepositoryReader.FilterType.INCLUDE,
+    ),
+).load_data(branch=branch)
+embed_model = OPEAEmbedding(
+    model_name="...",
+    api_base=openai_api_base,
+    api_key=openai_api_key,
+)
+print(documents)
+index = VectorStoreIndex.from_documents(documents, embed_model=embed_model)
+query_engine = index.as_query_engine()
+response = query_engine.query(
+    "What is the difference between VectorStoreIndex and SummaryIndex?",
+    verbose=True,
+)
+print(response)
+# chat = ChatOpenAI(model="gpt-4o", temperature=0.2, openai_api_base=openai_api_base)
+# from langchain_chroma import Chroma
+# from langchain_openai import OpenAIEmbeddings
 
-loader = WebBaseLoader("https://docs.smith.langchain.com/overview")
-data = loader.load()
-from langchain_text_splitters import RecursiveCharacterTextSplitter
+# vectorstore = Chroma.from_documents(documents=documents, embedding=OpenAIEmbeddings())
+# # k is the number of chunks to retrieve
+# retriever = vectorstore.as_retriever(k=4)
 
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
-all_splits = text_splitter.split_documents(data)
-from langchain_chroma import Chroma
-from langchain_openai import OpenAIEmbeddings
-
-vectorstore = Chroma.from_documents(documents=all_splits, embedding=OpenAIEmbeddings())
-# k is the number of chunks to retrieve
-retriever = vectorstore.as_retriever(k=4)
-
-docs = retriever.invoke("Can LangSmith help test my LLM applications?")
-print(docs)
+# docs = retriever.invoke("What is the difference between VectorStoreIndex and SummaryIndex?")
+# print(docs)
 # from langchain.chains.combine_documents import create_stuff_documents_chain
 # from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 
